725122c6f6fb18d447b0a0d48705a40882e2c2d0
Manishi <manishirani30501@gmail.com>
2023-09-25 15:11:48 +0530

    deployment change

--- Backend/server.js
+++ Backend/server.js
@@ -1,36 +1,44 @@
-//importing pakages of express from node or pakages
-const express= require("express");
-const dotenv = require('dotenv'); // inporting env file
-const notes = require("./data/notes");
-const connectDB= require("./config/db");
+const express = require("express");
+const body_parser = require("body-parser");
+const dotenv = require('dotenv');
+const connectDB = require('./config/db')
 const userRoutes = require("./routes/userRoutes");
 const noteRoutes = require("./routes/noteRoutes");
 const { notFound, errorHandler } = require("./middlewares/errorMiddleware");
+const path = require("path")
 
-
-//creating object of the imported pakage 
-const app=express();
+const app = express();
+app.use(body_parser.urlencoded({ extended: true }));
 dotenv.config();
 connectDB();
 app.use(express.json());
- 
-//app.get brings the data from the backend to the frontend and serves it
-app.get('/', (req,res)=>{
-    res.send("API is running...");
-});
-
-// app.get("/api/notes", (req,res)=>{
-//     res.json(notes);
-// })
 
 app.use("/api/users", userRoutes)
 app.use("/api/notes", noteRoutes)
 
+// --------------------------deployment------------------------------
+// __dirname = path.resolve();
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "../frontend/build")));
+
+  app.get("*", (req, res) =>
+    // res.sendFile(path.resolve(__dirname, "frontend", "build", "index.html"))
+    res.sendFile(path.join(__dirname, "../frontend/build/index.html"))
+  );
+} else {
+  app.get("/", (req, res) => {
+    res.send("API is running..");
+  });
+}
+// --------------------------deployment------------------------------
+
+// Error Handling middlewares
+
 app.use(notFound);
 app.use(errorHandler);
 
-const PORT =process.env.PORT || 5000;
+const PORT = process.env.PORT || 5000;
 
-//listen on a particular port which we r providing it
-app.listen(PORT,console.log(`server started at port ${PORT}`));
-
+app.listen(PORT, function () {
+  console.log(`Server is running on PORT ${PORT}`);
+});
